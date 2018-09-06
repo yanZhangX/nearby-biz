5b99c4ab557cb13274ec156f90b169b11e96e2eb
yue <407052885@qq.com>
2018-09-06 14:39:15 +0800

    upload

--- src/views/stock/show/index.vue
+++ src/views/stock/show/index.vue
@@ -22,7 +22,7 @@
       <el-table :data="tableData" v-loading.body="loading" stripe max-height=2000 @row-click="">
         <el-table-column prop="bookingCustomerName" label="客户姓名" min-width="100" :formatter="bookingCustomerNameFormat"></el-table-column>
         <el-table-column prop="bookingCustomerPhoneNumber" label="客户手机" min-width="150" :formatter="bookingCustomerPhoneNumberFormat"></el-table-column>
-        <el-table-column prop="code" label="电子码" min-width="140" v-if="operation === 'complete'"></el-table-column>
+        <el-table-column prop="code" label="电子码" min-width="140" v-if="operation === 'complete' || bookingShowCode() === 1"></el-table-column>
         <el-table-column prop="bookingDay" label="预约时间" min-width="200" :formatter="pro_yyyyMMDD" v-if="operation === 'booking'"></el-table-column>
         <el-table-column prop="completeDate" label="核销时间" min-width="200" :formatter="completeDate" v-if="operation === 'complete'"></el-table-column>
         <el-table-column prop="bookingMemo" label="备注" min-width="150"></el-table-column>
@@ -95,6 +95,16 @@
       }
     },
     methods: {
+      bookingShowCode () {
+        var bookingShowCode = getUser().bookingShowCode
+        if (bookingShowCode === null || bookingShowCode === 'undefined' || !bookingShowCode) {
+          return 0
+        }
+        if (bookingShowCode.toString() === '1') {
+          return 1
+        }
+        return 0
+      },
       paramIsNull (param) {
         if (typeof (param) === 'undefined' || param === null) {
           return true
