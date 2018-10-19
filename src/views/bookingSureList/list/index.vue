4939e86f6e84c54bedae770e707d1f6bd08301b4
mayongsong <409884483@qq.com>
2018-10-19 23:03:05 +0800

    商家后台确认列表支持搜索

--- src/views/bookingSureList/list/index.vue
+++ src/views/bookingSureList/list/index.vue
@@ -13,6 +13,22 @@
         <el-select v-model="type" placeholder="请选择类型" @change="bookingTypeChanged">
           <el-option v-for="item in bookingTypeList" :label="item.name" :value="item.id"></el-option>
         </el-select>
+        <div class="k-search-contaienr">
+          <el-date-picker type="daterange"
+                          :editable="false"
+                          v-model="selectDateSearch"
+                          range-separator="至"
+                          start-placeholder="开始日期"
+                          end-placeholder="结束日期"
+                          @change="search"></el-date-picker>
+        </div>
+        <div class="k-search-contaienr">
+          <i class="el-icon-search k-center" @click="search"></i>
+          <input type="text" placeholder="电子码、手机、姓名" class="k-search-input" v-model="keywords" @keyup.enter="search" v-focus>
+        </div>
+      </div>
+      <div class="r">
+        <el-button class="uploader" type="primary" @click="exportExcelSelectDate">导出预约</el-button>
       </div>
     </div>
     <div class="main-container">
@@ -21,7 +37,11 @@
         <el-table-column prop="bookingCustomerPhoneNumber" label="客户手机" min-width="150" :formatter="bookingCustomerPhoneNumberFormat"></el-table-column>
         <el-table-column prop="bookingDay" label="预约时间" min-width="100" :formatter="pro_yyyyMMDD"></el-table-column>
         <el-table-column prop="bookingItemText" label="预约类型" min-width="100"></el-table-column>
-        <el-table-column prop="memo" label="预约备注" min-width="100"></el-table-column>
+        <el-table-column prop="bookingMemo" label="预约备注" min-width="100">
+          <template slot-scope="scope">
+            <span>{{scope.row.bookingMemo}}</span>&nbsp;&nbsp;<span>{{scope.row.memo}}</span>
+          </template>
+        </el-table-column>
         <el-table-column prop="createDate" label="确定时间" min-width="160">
           <template slot-scope="scope">
             <span v-html="pro_yyyyMMDD(null, null, scope.row.createDate)" v-if="!paramIsNull(scope.row.sure) && scope.row.sure === 1"></span>
@@ -44,9 +64,11 @@
 
 <script>
   import moment from 'moment'
+  import {appHost, getToken} from 'CONST'
   export default {
     data () {
       return {
+        keywords: null,
         tableData: null,
         currentPage: 1,
         pageSize: 15,
@@ -68,7 +90,8 @@
         bookingItem: {
           id: null,
           name: null
-        }
+        },
+        selectDateSearch: null
       }
     },
     created () {
@@ -121,14 +144,34 @@
           return ''
         }
       },
+      search () {
+        this.currentPage = 1
+        this.getTableData()
+      },
       getTableData () {
+        var loading = this.$loading({
+          lock: true,
+          text: '数据加载中……',
+          spinner: 'el-icon-loading',
+          background: 'rgba(0, 0, 0, 0.7)'
+        })
+        var beginDate = null
+        var endDate = null
+        if (!this.paramIsNull(this.selectDateSearch)) {
+          beginDate = this.selectDateSearch[0].getTime()
+          endDate = this.selectDateSearch[1].getTime()
+        }
         this.$http.get(`/v1/a/biz/order/sure/list?type=${this.type}`, {
           params: {
             pageSize: this.pageSize,
             pageIndex: this.currentPage,
-            bookingItemId: this.bookingItem.id
+            bookingItemId: this.bookingItem.id,
+            search: this.keywords,
+            beginDate: beginDate,
+            endDate: endDate
           }
         }).then(res => {
+          loading.close()
           if (res.body.errMessage) {
             this.$message.error(res.body.errMessage)
           } else {
@@ -137,6 +180,7 @@
             this.pageCount = res.body.data.pageCount
           }
         }).catch(res => {
+          loading.close()
           this.$message.error('服务器繁忙！')
         })
       },
@@ -159,7 +203,14 @@
           type: 'warning',
           center: true
         }).then(() => {
+          var loading = this.$loading({
+            lock: true,
+            text: '设置中……',
+            spinner: 'el-icon-loading',
+            background: 'rgba(0, 0, 0, 0.7)'
+          })
           this.$http.post(`/v1/a/biz/order/sure?orderId=${row.orderId}`).then(res => {
+            loading.close()
             if (res.body.errMessage) {
               this.$message.error(res.body.errMessage)
             } else {
@@ -167,6 +218,7 @@
               this.getTableData()
             }
           }).catch(e => {
+            loading.close()
             this.$message.error('服务器错误')
           })
         })
@@ -174,6 +226,42 @@
       bookingTypeChanged () {
         this.currentPage = 1
         this.getTableData()
+      },
+      exportExcelSelectDate () {
+        if (this.paramIsNull(this.tableData)) {
+          this.pro_message_error(null, '没有数据可导出')
+          return
+        }
+        var h = this.$createElement
+        this.$msgbox({
+          title: '温馨提示',
+          message: h('p', null, [
+            h('span', null, '确认要导出数据吗？')
+          ]),
+          showCancelButton: true,
+          confirmButtonText: '确认导出',
+          cancelButtonText: '再考虑一下',
+          type: 'warning',
+          center: true
+        }).then(() => {
+          var beginDate = 0
+          var endDate = 0
+          if (!this.paramIsNull(this.selectDateSearch)) {
+            beginDate = this.selectDateSearch[0].getTime()
+            endDate = this.selectDateSearch[1].getTime()
+          }
+          var bookingItemId = 0
+          if (!this.paramIsNull(this.bookingItem.id)) {
+            bookingItemId = this.bookingItem.id
+          }
+          console.log(bookingItemId)
+          var search = ''
+          if (!this.paramIsNull(this.keywords)) {
+            search = this.keywords
+          }
+          var url = `${appHost()}/v1/a/booking/sure/list?bookingItemId=${bookingItemId}&token=${getToken()}&search=${search}&type=${this.type}&beginDate=${beginDate}&endDate=${endDate}`
+          window.open(url)
+        }).catch(e => {})
       }
     }
   }
